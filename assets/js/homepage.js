47b178c803d4853f54dbe68f37b39eab5bfa8241
Jonathan Cano <jcano528@gmail.com>
2021-08-16 19:01:19 -0500

    added some pseudocode to get the ball rolling

---
+++ assets/js/homepage.js
@@ -0,0 +1,9 @@
+// declare the variables:
+// * div container that holds the dynamically created time blocks
+
+
+// create a funtion using jQuery to  create and append the current date to the header section
+// set classes to match the font in the header
+// format the date using moment.js syntax
+
+// create a function that will implement 
