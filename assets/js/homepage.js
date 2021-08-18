2040706b06558501512a6172439a18f84e8c53b5
Jonathan Cano <jcano528@gmail.com>
2021-08-18 12:39:56 -0500

    Added moment variable, implemented jQuery, rendered current day on homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,11 @@
 // declare the variables:
 // * currentDay container
 // * div container that holds the dynamically created time blocks
-
+// current time in moment
+var moment = moment();
 
 // create a funtion using jQuery to  create and append the current date to the header section on page load
+$("p#currentDay").append(moment.format("dddd[,] MMMM Do"));
 // * set classes to match the font in the header
 // * loop to fill the hours of the day with a condition to allow access to hours in the current day only using mom=nt.js
 // * format the date using moment.js syntax
