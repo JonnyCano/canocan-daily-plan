03bbd2ce4ccb955f6bce3acc7dd67962c2d85c08
Jonathan Cano <jcano528@gmail.com>
2021-08-18 04:04:58 -0500

    created pseudocode to get an idea of what the app will be like

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,11 @@
 // declare the variables:
+// * currentDay container
 // * div container that holds the dynamically created time blocks
 
 
-// create a funtion using jQuery to  create and append the current date to the header section
-// set classes to match the font in the header
-// format the date using moment.js syntax
+// create a funtion using jQuery to  create and append the current date to the header section on page load
+// * set classes to match the font in the header
+// * loop to fill the hours of the day with a condition to allow access to hours in the current day only using mom=nt.js
+// * format the date using moment.js syntax
 
 // create a function that will implement 
